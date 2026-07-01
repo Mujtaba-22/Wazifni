4297cd3f90599de9af8c54f625f8363e12f1597c
Saadradh <S443005921@st.uqu.edu.sa>
2026-07-01 21:30:41 +0300

    تجربه

--- obj/Release/net10.0/Wazifni.AssemblyInfo.cs
+++ obj/Release/net10.0/Wazifni.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wazifni")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a97f128a472928f1a52cee18a75c8f1e303de3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wazifni")]
